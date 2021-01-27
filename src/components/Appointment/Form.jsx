aa72e63d45d68e28ed80256c1a559afc8caac5c5
CD-Mackay <connor.mackay@gmail.com>
2021-01-27 16:19:39 -0500

    refactor

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -36,6 +36,10 @@ export default function Form (props) {
     if (name === "") {
       setError("Student name cannot be blank");
       return
+    }
+    if (!interviewer) {
+      setError("Interviewer must be selected")
+      return
     }  
       setError("");
       props.onSave(name, interviewer);
